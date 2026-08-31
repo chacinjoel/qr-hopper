2ad833668c63594bb3a8d37d96dee14cc14d5465
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-08-31 04:54:19 -0400

    Cache v0.5.2 repair pre-roll

--- hopperlink-pixelstream/sw.js
+++ hopperlink-pixelstream/sw.js
@@ -1,5 +1,5 @@
-const CACHE='hopperlink-pixelstream-v051';
-const ASSETS=['./','./index.html','./styles.css?v=051','./fiducial-filter.css?v=051','./fiducial-filter.js?v=051','./hps5.js?v=051','./manifest.json'];
+const CACHE='hopperlink-pixelstream-v052';
+const ASSETS=['./','./index.html','./styles.css?v=052','./fiducial-filter.css?v=052','./fiducial-filter.js?v=052','./hps5.js?v=052','./repair-preroll.js?v=052','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
