fef07958569799613e97833e0cb7e0d83dfe2aec
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-08-31 05:03:29 -0400

    Bump cache for first repair frame lock v0.5.3

--- hopperlink-pixelstream/sw.js
+++ hopperlink-pixelstream/sw.js
@@ -1,5 +1,5 @@
-const CACHE='hopperlink-pixelstream-v052';
-const ASSETS=['./','./index.html','./styles.css?v=052','./fiducial-filter.css?v=052','./fiducial-filter.js?v=052','./hps5.js?v=052','./repair-preroll.js?v=052','./manifest.json'];
+const CACHE='hopperlink-pixelstream-v053';
+const ASSETS=['./','./index.html','./styles.css?v=053','./fiducial-filter.css?v=053','./fiducial-filter.js?v=053','./hps5.js?v=053','./repair-preroll.js?v=053','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
