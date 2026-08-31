991eb4be7da1877cc0d5dec12cf67cd445525e69
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-08-31 04:11:35 -0400

    Bump PixelStream cache to v0.4.4

--- hopperlink-pixelstream/sw.js
+++ hopperlink-pixelstream/sw.js
@@ -1,5 +1,5 @@
-const CACHE='hopperlink-pixelstream-v043';
-const ASSETS=['./','./index.html','./styles.css?v=043','./manual-arq.js?v=043','./color-assist.js?v=043','./pass-hold.js?v=043','./hps4.js?v=043','./manifest.json'];
+const CACHE='hopperlink-pixelstream-v044';
+const ASSETS=['./','./index.html','./styles.css?v=044','./manual-arq.js?v=044','./color-assist.js?v=044','./pass-hold.js?v=044','./hps4-session-loader.js?v=044','./hps4.js?v=044core','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
   self.clients.claim(),
