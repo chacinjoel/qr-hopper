b88f4909511a34acc5377ae5c9ebd4b8a603be3f
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-08-31 04:12:15 -0400

    Bump PixelStream cache to v0.4.5

--- hopperlink-pixelstream/sw.js
+++ hopperlink-pixelstream/sw.js
@@ -1,8 +1,5 @@
-const CACHE='hopperlink-pixelstream-v044';
-const ASSETS=['./','./index.html','./styles.css?v=044','./manual-arq.js?v=044','./color-assist.js?v=044','./pass-hold.js?v=044','./hps4-session-loader.js?v=044','./hps4.js?v=044core','./manifest.json'];
+const CACHE='hopperlink-pixelstream-v045';
+const ASSETS=['./','./index.html','./styles.css?v=045','./manual-arq.js?v=045','./color-assist.js?v=045','./pass-hold.js?v=045','./hps4-session-loader.js?v=045','./hps4.js?v=044core','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(Promise.all([
-  self.clients.claim(),
-  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-])));
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
