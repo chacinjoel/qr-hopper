685feb1b94f95abe0a89c58abb0817a231185a82
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-08-31 02:37:59 -0400

    Bump PixelStream service worker cache to v0.2

--- hopperlink-pixelstream/sw.js
+++ hopperlink-pixelstream/sw.js
@@ -1,5 +1,8 @@
-const CACHE='hopperlink-pixelstream-v01';
+const CACHE='hopperlink-pixelstream-v02';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
+self.addEventListener('activate',e=>e.waitUntil(Promise.all([
+  self.clients.claim(),
+  caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
