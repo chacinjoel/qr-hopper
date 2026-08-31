4c670da43bad42da2143a981e0e75877aac845b8
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-08-31 03:52:42 -0400

    Bump HopperLink cache to v0.4.2 with Color Assist

--- hopperlink-pixelstream/sw.js
+++ hopperlink-pixelstream/sw.js
@@ -1,5 +1,5 @@
-const CACHE='hopperlink-pixelstream-v041';
-const ASSETS=['./','./index.html','./styles.css?v=041','./manual-arq.js?v=041','./hps4.js?v=041','./manifest.json'];
+const CACHE='hopperlink-pixelstream-v042';
+const ASSETS=['./','./index.html','./styles.css?v=042','./manual-arq.js?v=042','./color-assist.js?v=042','./hps4.js?v=042','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([
   self.clients.claim(),
