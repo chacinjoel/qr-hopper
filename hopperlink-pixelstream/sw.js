94d23618db7d60dd0b96698ea3b12d818acf1296
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-08-31 18:44:56 -0400

    Cache HPS6 camera fallback

--- hopperlink-pixelstream/sw.js
+++ hopperlink-pixelstream/sw.js
@@ -1,5 +1,5 @@
 const CACHE='hopperlink-pixelstream-v060';
-const ASSETS=['./','./index.html','./styles.css?v=060','./fiducial-filter.css?v=060','./fiducial-filter.js?v=060','./hps6.js?v=060','./manifest.json'];
+const ASSETS=['./','./index.html','./styles.css?v=060','./camera-fallback.js?v=060','./fiducial-filter.css?v=060','./fiducial-filter.js?v=060','./hps6.js?v=060','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
