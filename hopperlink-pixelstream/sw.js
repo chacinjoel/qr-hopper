b21e978a9b9c4ac74d2594846de86a99814a52d5
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-08-31 04:30:20 -0400

    Cache HPS5 StateFlow assets

--- hopperlink-pixelstream/sw.js
+++ hopperlink-pixelstream/sw.js
@@ -1,5 +1,5 @@
-const CACHE='hopperlink-pixelstream-v046';
-const ASSETS=['./','./index.html','./styles.css?v=046','./manual-arq.js?v=046','./color-assist.js?v=046','./pass-hold.js?v=046','./hps4-session-loader.js?v=046','./hps4.js?v=046core','./manifest.json'];
+const CACHE='hopperlink-pixelstream-v050';
+const ASSETS=['./','./index.html','./styles.css?v=050','./hps5.js?v=050','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
