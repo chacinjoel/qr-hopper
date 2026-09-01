3871b6a2fb5b494593c309d1f6769e2c3c703117
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-09-01 01:20:32 -0400

    Cache v0.8.2 closing burst telemetry assets

--- hopperlink-pixelstream/sw.js
+++ hopperlink-pixelstream/sw.js
@@ -1,5 +1,5 @@
-const CACHE='hopperlink-pixelstream-v081';
-const ASSETS=['./','./index.html','./styles.css?v=081','./photometric-lock.css?v=081','./binary-stage.css?v=081','./binary-tag-bridge.js?v=081','./binary-tag-status.js?v=081','./safe-color-constellation.js?v=081','./hps7.js?v=081','./color-warmup.js?v=081','./manifest.json'];
+const CACHE='hopperlink-pixelstream-v082';
+const ASSETS=['./','./index.html','./styles.css?v=082','./photometric-lock.css?v=082','./binary-stage.css?v=082','./binary-tag-bridge.js?v=082','./binary-tag-status.js?v=082','./safe-color-constellation.js?v=082','./hps7-runtime.js?v=082','./hps7.js?v=082core','./color-warmup.js?v=082','./transfer-telemetry.js?v=082','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
