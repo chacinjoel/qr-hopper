a4c8080596ce0d02ad102c8bf8f28967f9c01881
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-09-01 02:55:26 -0400

    Force v0.9.0 acoustic cache refresh

--- hopperlink-pixelstream/sw.js
+++ hopperlink-pixelstream/sw.js
@@ -1,4 +1,4 @@
-const CACHE='hopperlink-pixelstream-v090';
+const CACHE='hopperlink-pixelstream-v090a';
 const ASSETS=['./','./index.html','./styles.css?v=090','./photometric-lock.css?v=090','./binary-stage.css?v=090','./binary-tag-bridge.js?v=090','./binary-tag-status.js?v=090','./safe-color-constellation.js?v=090','./acoustic-control.js?v=090','./hps7-runtime.js?v=090','./hps7.js?v=090core','./color-warmup.js?v=090','./transfer-telemetry.js?v=090','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
