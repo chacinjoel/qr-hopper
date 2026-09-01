275d5a6c9bb194dad33def78981a9b0bc80706fc
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-08-31 23:46:47 -0400

    Cache HPS7.2 color training and geometry guard

--- hopperlink-pixelstream/sw.js
+++ hopperlink-pixelstream/sw.js
@@ -1,5 +1,5 @@
-const CACHE='hopperlink-pixelstream-v071';
-const ASSETS=['./','./index.html','./styles.css?v=071','./photometric-lock.css?v=071','./hps7.js?v=071','./manifest.json'];
+const CACHE='hopperlink-pixelstream-v072';
+const ASSETS=['./','./index.html','./styles.css?v=072','./photometric-lock.css?v=072','./corner-geometry-guard.js?v=072','./hps7.js?v=072','./color-warmup.js?v=072','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
