05134845cfacc2370c69beacdd3078ba0c405042
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-08-31 20:18:48 -0400

    Bump HPS7 cache to v0.7.1

--- hopperlink-pixelstream/sw.js
+++ hopperlink-pixelstream/sw.js
@@ -1,5 +1,5 @@
-const CACHE='hopperlink-pixelstream-v070';
-const ASSETS=['./','./index.html','./styles.css?v=070','./photometric-lock.css?v=070','./hps7.js?v=070','./manifest.json'];
+const CACHE='hopperlink-pixelstream-v071';
+const ASSETS=['./','./index.html','./styles.css?v=071','./photometric-lock.css?v=071','./hps7.js?v=071','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
