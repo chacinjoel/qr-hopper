bd8892a2841179eb770f0cdbded62aaaa3690bb4
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-09-01 00:32:14 -0400

    Publish v0.8 binary stage cache

--- hopperlink-pixelstream/sw.js
+++ hopperlink-pixelstream/sw.js
@@ -1,5 +1,5 @@
-const CACHE='hopperlink-pixelstream-v075';
-const ASSETS=['./','./index.html','./styles.css?v=075','./photometric-lock.css?v=075','./monochrome-fiducials.css?v=075','./corner-geometry-guard.js?v=075','./safe-color-constellation.js?v=075','./hps7.js?v=075','./color-warmup.js?v=075','./manifest.json'];
+const CACHE='hopperlink-pixelstream-v080';
+const ASSETS=['./','./index.html','./styles.css?v=080','./photometric-lock.css?v=080','./binary-stage.css?v=080','./binary-tag-bridge.js?v=080','./safe-color-constellation.js?v=080','./hps7.js?v=080','./color-warmup.js?v=080','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
