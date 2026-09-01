f5dae0fc2e20b10992f295817223f5fbf00dd432
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-08-31 20:14:04 -0400

    Cache HPS7 adaptive optical modem

--- hopperlink-pixelstream/sw.js
+++ hopperlink-pixelstream/sw.js
@@ -1,5 +1,5 @@
-const CACHE='hopperlink-pixelstream-v060';
-const ASSETS=['./','./index.html','./styles.css?v=060','./camera-fallback.js?v=060','./fiducial-filter.css?v=060','./fiducial-filter.js?v=060','./hps6.js?v=060','./manifest.json'];
+const CACHE='hopperlink-pixelstream-v070';
+const ASSETS=['./','./index.html','./styles.css?v=070','./photometric-lock.css?v=070','./hps7.js?v=070','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(Promise.all([self.clients.claim(),caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))])));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
