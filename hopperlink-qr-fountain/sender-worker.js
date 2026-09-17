8c52caa564615e5dabd97151bbeffd754258ea4d
chacinjoel <78094439+chacinjoel@users.noreply.github.com>
2026-09-17 19:24:41 -0400

    Bump dual QR browser cache to qf08

--- hopperlink-qr-fountain/sender-worker.js
+++ hopperlink-qr-fountain/sender-worker.js
@@ -1,4 +1,4 @@
-import {FountainEncoder} from './fountain.js?v=qf07';
+import {FountainEncoder} from './fountain.js?v=qf08';
 let writer=null,encoder=null,qrVersion=40,ecLevel='L';
 async function loadWriter(){if(writer)return writer;const mod=await import('./vendor/zxing/es/writer/index.js');mod.prepareZXingModule({overrides:{locateFile:path=>new URL('./vendor/zxing/writer/'+path,self.location.href).href}});writer=mod;return writer;}
 self.onmessage=async e=>{const m=e.data;try{
